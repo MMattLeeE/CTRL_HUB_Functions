7a17aa88d97cf25b4b8e791a3b88604ad2fe3180
MMattLeeE <mmattleee@gmail.com>
2021-07-31 23:06:57 -0400

    update gitignore

--- functions/index.js
+++ functions/index.js
@@ -42,8 +42,9 @@ exports.makeUppercase = functions.firestore.document('/messages/{documentId}')
 });
 
 exports.testFunction = functions.https.onCall((data, context) => {
-  //expecting the client to send {text: some text}
-  const text = data;
-  newText = 'data sent to server: ' + data;
+  //expecting the client to send a map/json {text: some text}
+  const text = data.text;
+  functions.logger.log('what we got from client: '+ text)
+  newText = 'data sent from server: ' + text;
   return {text: newText}
 });
